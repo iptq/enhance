de14ac12889ca42544339a78cb1860eadfe52883
Michael Zhang <failed.down@gmail.com>
2015-03-05 13:17:21 -0600

    Initial commit.

---
+++ ga.fxyz.enhance/Enhance.java
@@ -0,0 +1,13 @@
+/**
+ * Enhance
+ * Created by Michael Zhang.
+ */
+public class Enhance {
+	/**
+	 * Entry point for the program.
+	 * @param args Command-line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
